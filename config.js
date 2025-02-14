6d3176f595d68f77a3fb89534013797fe6de88f5
Ajsjns <asbhhjagshgashja@gmail.com>
2025-02-14 22:09:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["967781597426", '〔⚡️〕𝑻𝑬𝑹𝑩𝑶〔⚡️〕', true],
+["967783179256", '〔⚡️〕𝑻𝑬𝑹𝑩𝑶〔⚡️〕', true],
 ["5573031859"],
 ["5218715746374"],
 ["593968585383"],
